2fecbfc770b1024c4133ffb4fd5f1f500d01dc60
Rodrigo Pires Meira <rpmeir@gmail.com>
2022-03-21 20:34:59 -0300

    chore: Add namespaces

---
+++ src/Dollar.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+class Dollar {
+    public float $amount;
+
+    public function __construct(float $number)
+    {
+        $this->amount = $number;
+    }
+
+    public function times(float $number): void {
+        $this->amount *= $number;
+    }
+}
---
+++ tests/DollarTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests;
+
+use App\Dollar;
+
+test('Deve retornar 10 quando multiplicar 5 por 2', function () {
+    $five = new Dollar(5);
+    $five->times(2);
+    expect($five->amount)->toEqual(10);
+});
