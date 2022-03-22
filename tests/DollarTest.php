74d58584ca3d117e3333ccd6b2cc7037b495f0d8
Rodrigo Pires Meira <rpmeir@gmail.com>
2022-03-21 21:33:27 -0300

    chore: Define PHP PSR12 code standard

--- tests/DollarTest.php
+++ tests/DollarTest.php
@@ -5,7 +5,8 @@ namespace Tests;
 use App\Dollar;
 
 test(
-    'Deve retornar 10 quando multiplicar 5 por 2', function () {
+    'Deve retornar 10 quando multiplicar 5 por 2',
+    function () {
         $five = new Dollar(5);
         $five->times(2);
         expect($five->amount)->toEqual(10);
